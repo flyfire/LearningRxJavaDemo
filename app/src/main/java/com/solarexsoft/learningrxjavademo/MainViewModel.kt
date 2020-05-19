1be74a782bd8aba2e29fb5aef4fd83baf84b85aa
Solarex <rh.hou.work@gmail.com>
2020-05-19 17:28:40 +0800

    thread

--- app/src/main/java/com/solarexsoft/learningrxjavademo/MainViewModel.kt
+++ app/src/main/java/com/solarexsoft/learningrxjavademo/MainViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import kotlin.concurrent.thread
 
 /**
  * Created by houruhou on 2020/5/19/5:03 PM
