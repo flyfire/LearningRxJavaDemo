fd242228797f56aea21e78ebbd47cf9a4e93c3ef
Solarex <rh.hou.work@gmail.com>
2020-05-26 21:22:31 +0800

    in/out

--- app/src/main/java/com/solarexsoft/learningrxjavademo/InOut.kt
+++ app/src/main/java/com/solarexsoft/learningrxjavademo/InOut.kt
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
  * Created by houruhou on 2020/5/26/8:55 PM
  * Desc:
  */
+// https://www.kotlincn.net/docs/reference/generics.html
+// https://kaixue.io/kotlin-generics/
 fun main() {
     val adapter:RecyclerView.Adapter<out RecyclerView.ViewHolder> = InOutJava.MyAdapter()
 }
